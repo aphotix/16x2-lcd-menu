a5eb0159371431db17de74ea6076eeb3e90e1d06
David <aphotix@gmail.com>
2019-01-01 03:42:27 -0500

    Oops, this isn't react
    * Numerous bug fixes
    * Added example
    * Not really functional yet

---
+++ examples/demo.js
@@ -0,0 +1,76 @@
+const adaLcd = require('../lib/16x2_adafruit_lcd.js');
+const LCDMENU = new adaLcd.LCDMENU(60);
+
+var num = 1;
+
+const incrementNum = () => {
+  num++;
+  return;
+}
+
+const decrementNum = () => {
+  num--;
+}
+
+const getNum = () => {num};
+
+const menus = [
+  {
+    id: 'topLevel',
+    name: 'topLevel',
+    enabled: true,
+    actions: {
+      right: 'secondaryMenu',
+    },
+    template: [
+      'Welcome to',
+      'LCDMENU',
+    ]
+  },
+  {
+    id: 'menuDown',
+    name: 'menuDown',
+    enabled: true,
+    actions: {
+      up: incrementNum,
+      down: decrementNum,
+      select: 'topLevel',
+      left: 'topLevel',
+    },
+    data: getNum,
+    template: ['Current Num: ${num}', '< Left to exit'],
+  },
+  {
+    id: 'secondaryMenu',
+    name: 'secondaryMenu',
+    enabled: true,
+    actions: {
+      left: 'topLevel',
+    },
+    template: [
+      'Line 1',
+      'Line 2',
+      'Line 3',
+      'Line 4',
+      'Line 5',
+    ],
+  },
+];
+
+
+menus.forEach((menu) => LCDMENU.addMenu(menu));
+
+LCDMENU.init();
+
+process.on('exit', () => {
+  console.log('exit')
+  adaLcd.lcd.clear();
+	adaLcd.lcd.backlight(adaLcd.lcd.colors.OFF);
+  adaLcd.lcd.close();
+});
+process.on('SIGINT', () => {
+  console.log('ctrl c')
+  adaLcd.lcd.clear();
+  adaLcd.lcd.backlight(adaLcd.lcd.colors.OFF);
+  adaLcd.lcd.close();
+});
